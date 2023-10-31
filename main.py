6ffda20bfcc509abff4b19702f9114e69698a2ce
anastasia-spb <anastasia.vlasova6293@gmail.com>
2023-10-31 11:45:55 +0300

    Add tuning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
 import detectron2
+import cv2, random, os
 
 # Some basic setup:
 # Setup detectron2 logger
@@ -12,6 +13,7 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data.datasets.coco import load_coco_json
 
 from PIL import Image
 import numpy as np
@@ -32,10 +34,76 @@ def print_versions():
     print("detectron2:", detectron2.__version__)
 
 
-def main(input_img_path: str = '/workspaces/detectron/data/hyena.coco/images/train2022/000000003036.jpg'):
+def load_data(annotation_file: str = '/workspaces/DetectronDemo/data/hyena.coco/annotations/instances_train2022.json'):
+    return load_coco_json(annotation_file, image_root='/workspaces/DetectronDemo/data/hyena.coco/images/train2022')
+
+def check_gt():
+    DatasetCatalog.register("hyenas_dataset", lambda: load_data())
+    MetadataCatalog.get("hyenas_dataset").set(thing_classes=["hyenas"])
+    hyenas_dataset = MetadataCatalog.get("hyenas_dataset")
+
+    dataset_dicts = load_data()
+    for d in random.sample(dataset_dicts, 3):
+        img = cv2.imread(d["file_name"])
+        visualizer = Visualizer(img[:, :, ::-1], metadata=hyenas_dataset, scale=0.5)
+        out = visualizer.draw_dataset_dict(d)
+        cv2.imshow("Check gt", out.get_image()[:, :, ::-1])
+        cv2.waitKey()
+
+    
+def transfer_from_coco_to_hyenas():
+    from detectron2.engine import DefaultTrainer
+
+    DatasetCatalog.register("hyenas_dataset", lambda: load_data())
+    MetadataCatalog.get("hyenas_dataset").set(thing_classes=["hyenas"])
+
+    cfg = get_cfg()
+    model_url = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+    cfg.merge_from_file(model_zoo.get_config_file(model_url))
+    cfg.DATASETS.TRAIN = ("hyenas_dataset",)
+    cfg.DATASETS.TEST = ()
+    cfg.DATALOADER.NUM_WORKERS = 8
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_url)  # Let training initialize from model zoo
+    cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
+    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+    cfg.SOLVER.MAX_ITER = 3000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+    cfg.SOLVER.STEPS = []        # do not decay learning rate
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). 
+
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    trainer = DefaultTrainer(cfg) 
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+
+    # torch.save(trainer.model.state_dict(), os.path.join(cfg.OUTPUT_DIR, "hyenas_checkpoint.pth"))
+    
+
+
+def run(input_img_path: str = '/workspaces/DetectronDemo/data/dog.jpg', only_coco: bool = True):
     print_versions()
     cfg = get_cfg()
-    predictor = load_model(cfg)
+    
+
+    if only_coco:
+        predictor = load_model(cfg)
+    else:
+        cfg = get_cfg()
+        model_url = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+        cfg.merge_from_file(model_zoo.get_config_file(model_url))
+        cfg.DATASETS.TRAIN = ("hyenas_dataset",)
+        cfg.DATASETS.TEST = ()
+        cfg.DATALOADER.NUM_WORKERS = 8
+        cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
+        cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+        cfg.SOLVER.MAX_ITER = 3000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+        cfg.SOLVER.STEPS = []        # do not decay learning rate
+        cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). 
+        cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set a custom testing threshold
+        predictor = DefaultPredictor(cfg)
+
     with Image.open(input_img_path) as input_image:
          input_array = np.array(input_image)
          outputs = predictor(input_array)
@@ -43,11 +111,13 @@ def main(input_img_path: str = '/workspaces/detectron/data/hyena.coco/images/tra
          v = Visualizer(input_array[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
          out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
          img = Image.fromarray(out.get_image()[:, :, ::-1].astype('uint8'), 'RGB')
-         img.save('temp_result.png', format="PNG")
+         img.save('temp_result300.png', format="PNG")
 
     print(outputs["instances"].pred_classes)
     print(outputs["instances"].pred_boxes)
          
 
 if __name__ == "__main__":
-    main()
+    run(only_coco=False)
+    # check_gt()
+    # transfer_from_coco_to_hyenas()
